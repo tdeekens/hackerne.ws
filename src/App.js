fe2434dc5e613fba033a2fbb4a8ac65a78594616
tdeekens <tobias.deekens@commercetools.de>
2018-11-05 18:36:33 +0100

    feat(now): add now deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './app.css';
 import React from 'react';
-import Route from 'react-router-dom/Route';
-import Switch from 'react-router-dom/Switch';
 import Header from './components/header';
 import Stories from './components/stories';
 
